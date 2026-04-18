32e105848f7a2aa70309d4c4cc8a8a971bc2f4b7
Szqore <84195482+Szqore@users.noreply.github.com>
2026-04-18 09:32:58 +0700

    Task16.java

--- Homework/1/1/Task16.java
+++ Homework/1/1/Task16.java
@@ -1,32 +1,32 @@
-import java.util.Random;
+import java.util.Random; //task 16
 
-class MatrixOperations {
+public class MatrixOperations {
 
     public static int[] findMinMaxInArray(int[] array) {
         int min = array[0];
         int max = array[0];
         for (int i = 1; i < array.length; i++) {
-            if (array[i] < min) min = array[i];
-            if (array[i] > max) max = array[i];
+            if (array[i] < min) min = array[i]; //обнволяем если мин нагли меньше
+            if (array[i] > max) max = array[i];//обнволяем если мах нагли больше
         }
-        return new int[]{min, max};
+        return new int[]{min, max}; //массив
     }
 
     public static int[] findMinMaxInMatrix(int[][] matrix) {
-        int globalMin = Integer.MAX_VALUE;
-        int globalMax = Integer.MIN_VALUE;
-        for (int[] row : matrix) {
-            int[] rowMinMax = findMinMaxInArray(row);
-            if (rowMinMax[0] < globalMin) globalMin = rowMinMax[0];
-            if (rowMinMax[1] > globalMax) globalMax = rowMinMax[1];
+        int globalMin = Integer.MAX_VALUE; // Начинаем с максимально возможного числа
+        int globalMax = Integer.MIN_VALUE; // Начинаем с минимально возможного числа
+        for (int[] row : matrix) {  // Перебираем каждую строку матрицы
+            int[] rowMinMax = findMinMaxInArray(row); // Находим min/max в строке
+            if (rowMinMax[0] < globalMin) globalMin = rowMinMax[0]; //обнв min
+            if (rowMinMax[1] > globalMax) globalMax = rowMinMax[1]; //обнв max
         }
         return new int[]{globalMin, globalMax};
     }
-
+// Метод для красивого вывода матрицы на экран
     public static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int value : row) {
-                System.out.printf("%4d", value);
+        for (int[] row : matrix) { // Проходим по строкам
+            for (int value : row) { // Проходим по элементам строки
+                System.out.printf("%4d", value); // Печатаем число, выделяя 4 позиции
             }
             System.out.println();
         }
@@ -35,7 +35,7 @@ class MatrixOperations {
     public static void main(String[] args) {
         Random rand = new Random();
         int[][] matrix = new int[4][5];
-        for (int i = 0; i < matrix.length; i++) {
+        for (int i = 0; i < matrix.length; i++) { // Проходим по столбцам
             for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = rand.nextInt(100);
             }
@@ -44,7 +44,7 @@ class MatrixOperations {
         System.out.println("Матрица:");
         printMatrix(matrix);
 
-        int[] result = findMinMaxInMatrix(matrix);
+        int[] result = findMinMaxInMatrix(matrix); // Находим min и max в матрице
         System.out.println("\nНаименьший элемент: " + result[0]);
         System.out.println("Наибольший элемент: " + result[1]);
     }
